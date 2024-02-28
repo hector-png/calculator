a996605ee84c9cb039a0af4c0328ce9e59c5d20c
Hector Atilano <159952379+daniel-Hector@users.noreply.github.com>
2024-02-27 19:26:44 -0700

    Add mathExpression object to seperate input

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const mathExpresion = {
     value1: '',
-
+    value2: '',
 };
 
 function handleButtonEvent(node) {
     let buttonClicked = node.innerText;
     const displayContent = document.querySelector('.display-text');
-    mathExpresion.value1 = mathExpresion.value1 + buttonClicked;
-    displayContent.innerText = mathExpresion.value1;
-    
+
+    if(node.className != 'operand' && !mathExpresion.operand){
+        mathExpresion.value1 = mathExpresion.value1 + buttonClicked;
+        displayContent.innerText = mathExpresion.value1;
+    } else if(node.className === 'operand' && !mathExpresion.operand) {
+        mathExpresion.operand = buttonClicked;
+        displayContent.innerText = `${mathExpresion.value1} ${mathExpresion.operand}`;
+    } else if(node.className != 'operand' && mathExpresion.operand) {
+        mathExpresion.value2 = mathExpresion.value2 + buttonClicked;
+        displayContent.innerText = `${mathExpresion.value1} ${mathExpresion.operand} ${mathExpresion.value2}`;
+    }
 }
 
-let buttons = document.querySelector('.buttons-container');
-buttons.addEventListener('click', (event) => {
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => button.addEventListener('click', (event) => {
     if(event.target.type != undefined) {
         handleButtonEvent(event.target);
     }
-});
+}));
